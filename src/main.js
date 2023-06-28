d9def2c2206fb132f8c8bc4617bc2843eb3a4893
hange <2498784547@qq.com>
2023-06-28 11:12:22 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
