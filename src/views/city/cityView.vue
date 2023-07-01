998775468589df0e31aba32953b7cc14123bbcbd
hange <2498784547@qq.com>
2023-07-01 22:50:18 +0800

    list部分布局完成

--- src/views/city/cityView.vue
+++ src/views/city/cityView.vue
@@ -2,16 +2,18 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list></city-list>
     </div>
 </template>
 
 <script>
 import cityHeader from './components/cityHeader.vue';
 import citySearch from './components/citySearch.vue';
+import cityList from './components/cityList.vue';
 
     export default {
         name:"cityView",
-        components:{cityHeader,citySearch}
+        components:{cityHeader,citySearch,cityList}
     }
 </script>
 
