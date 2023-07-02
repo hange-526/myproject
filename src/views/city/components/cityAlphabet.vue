3260faa6ad4ee678be7e6951a913275487a8678c
hange <2498784547@qq.com>
2023-07-02 09:32:42 +0800

    1

---
+++ src/views/city/components/cityAlphabet.vue
@@ -0,0 +1,40 @@
+<template>
+    <ul class="list" ref="wrapper">
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+    </ul>
+</template>
+
+<script>
+import Bscroll from 'better-scroll'
+
+    export default {
+        name:"cityAlphabet",
+        mounted(){
+            this.scroll=new Bscroll(this.$refs.wrapper)    //给当前获取的dom设置滚动工具
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.list
+    display: flex
+    flex-direction: column  //设置flex容器中子元素的排列方向为垂直方向
+    justify-content: center //在容器中水平居中对齐其子元素
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    .item
+        text-align: center
+        line-height: .4rem
+        color: $bgColor
+</style>
--- src/views/city/components/cityList.vue
+++ src/views/city/components/cityList.vue
@@ -1,75 +1,77 @@
 <template>
     <div class="list">
-        <div class="area">
-            <div class="title border-topbottom">当前城市</div>
-            <div class="button-list">
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
+        <div>
+            <div class="area">
+                <div class="title border-topbottom">当前城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="area">
-            <div class="title border-topbottom">热门城市</div>
-            <div class="button-list">
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
+            <div class="area">
+                <div class="title border-topbottom">热门城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="area">
-            <div class="title border-topbottom">A</div>
-            <div class="item-list">
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-            </div>
-            <div class="title border-topbottom">A</div>
-            <div class="item-list">
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-            </div>
-            <div class="title border-topbottom">A</div>
-            <div class="item-list">
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-            </div>
-            <div class="title border-topbottom">A</div>
-            <div class="item-list">
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
             </div>
         </div>
     </div>
