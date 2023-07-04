1a668784df6ec44aac0b0910e7588f110f44932c
hange <2498784547@qq.com>
2023-07-04 14:59:07 +0800

    递归列表完成

--- src/views/detail/detailView.vue
+++ src/views/detail/detailView.vue
@@ -2,16 +2,50 @@
     <div>
         <detail-banner></detail-banner>
         <detail-header></detail-header>
-        <div class="content"></div>
+        <div class="content">
+            <detail-list :list="list"></detail-list>
+        </div>
     </div>
 </template>
 
 <script>
 import detailBanner from './components.vue/detailBanner.vue';
 import detailHeader from './components.vue/detailHeader.vue';
+import detailList from './components.vue/detailList.vue';
     export default {
         name:"detailView",
-        components:{detailBanner,detailHeader}
+        data(){
+            return {
+                list:[
+                    {
+                        title:'成人票',
+                        children:[
+                            {
+                                title:"三馆联票",
+                                children:[
+                                    {
+                                        title:"三馆联票-某某连锁店销售"
+                                    }
+                                ]
+                            },
+                            {
+                                title:"五馆联票"
+                            },
+                        ]
+                    },
+                    {
+                        title:'学生票'
+                    },
+                    {
+                        title:'儿童票'
+                    },
+                    {
+                        title:'特惠票'
+                    },
+                ]
+            }
+        },
+        components:{detailBanner,detailHeader,detailList}
     }
 </script>
 
